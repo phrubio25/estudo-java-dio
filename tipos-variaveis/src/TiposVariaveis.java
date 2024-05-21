fbaeede69a4b018bc31f8f3ae5a7cac7bd1e170c
Pedro Rubio <ph.bragarubio@gmail.com>
2024-05-21 20:39:35 -0300

    lição 1

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,10 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        byte idade = 123;
+        short ano = 2024;
+        int cep = 12345678;
+        long cpf = 12345678989l;   //é necessário terminar com 'l' seja maiusculo ou minusculo, para que ele não seja considerado um inteiro.
+        float pi = 3.14f;    //é necessário terminar com 'F' seja maiusculo ou minusculo, para que não haja confusão com o double.
+        double salario = 5000.30;
     }
 }
