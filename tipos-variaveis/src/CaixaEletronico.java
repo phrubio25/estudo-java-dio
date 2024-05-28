a4e93d0c35618661effc33de7cedb67eb72ce367
Pedro Rubio <ph.bragarubio@gmail.com>
2024-05-27 21:40:29 -0300

    condição composta

---
+++ tipos-variaveis/src/CaixaEletronico.java
@@ -0,0 +1,16 @@
+public class CaixaEletronico {
+    public static void main(String[] args) {
+        double saldo = 25.0;
+        double valorSolicitado = 30.0;
+
+        if (valorSolicitado < saldo) {
+
+            saldo = saldo - valorSolicitado;
+
+            System.out.println(saldo);
+        }
+        else {
+            System.out.println("saldo insuficiente");
+        }
+    }
+}
